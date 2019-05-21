ceaeab39e3613563616e7a3cf71e0b3c46c9b87d
Maël Theulière <mael.theuliere@gmail.com>
2019-05-21 02:28:26 +0200

    - ajout définition d'un spatial df à partir d'un df

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -2,7 +2,9 @@
 # Contruction de la table des départements --------------------------------
 
 library(sf)
-
+library(stringr)
+library(readr)
+library(readxl)
 epci_geo<-st_read(dsn="extdata/epci/",layer="EPCI")
 
 departements_geo<-st_read(dsn="extdata/departements/",layer="DEPARTEMENT") %>% 
@@ -10,5 +12,19 @@ departements_geo<-st_read(dsn="extdata/departements/",layer="DEPARTEMENT") %>%
 
 regions_geo<-st_read(dsn="extdata/regions/",layer="REGION")
 
+prefecture_de_region_geo<-st_read(dsn="extdata/adminexpress/",layer="CHEF_LIEU") %>% 
+  filter(STATUT == "Préfecture de région") %>% 
+  mutate(coords=as.character(geometry) %>% 
+           str_replace_all("c\\(","") %>% 
+           str_replace_all("\\)","")) %>% 
+  mutate(x=str_split_fixed(coords,", ",2)[,1] %>% 
+           str_trim(),
+         y=str_split_fixed(coords,", ",2)[,2] %>% 
+           str_trim()
+         ) %>% 
+  select(-coords) %>% 
+  st_drop_geometry()
+
+write_csv2(prefecture_de_region_geo,"extdata/prefecture.csv")
 
 save(epci_geo,departements_geo,regions_geo,file="data/admin_express.RData")
